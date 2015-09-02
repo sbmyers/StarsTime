add223e028ac59d565d30387e14dae0ac25878b1
sbmyers <s.myers@computer.org>
2015-09-02 04:19:03 -0500

    Removed UI markers

--- src/adjust_time.c
+++ src/adjust_time.c
@@ -1,11 +1,10 @@
 #include <pebble.h>
 #include "adjust_time.h"
 
-// BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
 static Window *s_window;
 static GFont s_res_bitham_42_bold;
 static GFont s_res_gothic_18_bold;
-static GFont s_res_bitham_30_black;
+//static GFont s_res_bitham_30_black;
 static TextLayer *s_textlayer_title;
 static TextLayer *s_textlayer_subtitle;
 static TextLayer *s_textlayer_time;
@@ -16,15 +15,39 @@ static char s_Destination[4];
 static char s_Station[16];
 static char s_DayOfWeek[10];
 
+static void up_single_click_handler(ClickRecognizerRef recognizer, void *context) 
+{
+
+  //Window *window = (Window *)context;
+}
+
+static void select_single_click_handler(ClickRecognizerRef recognizer, void *context) 
+{
+
+  //Window *window = (Window *)context;
+}
+
+static void down_single_click_handler(ClickRecognizerRef recognizer, void *context) 
+{
+
+  //Window *window = (Window *)context;
+}
+static void config_provider(Window *window) 
+{
+ // single click / repeat-on-hold config:
+  window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_single_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_single_click_handler);
+}
 static void initialise_ui(void) {
   s_window = window_create();
-  #ifndef PBL_SDK_3
-    window_set_fullscreen(s_window, true);
-  #endif
+#ifndef PBL_SDK_3
+  window_set_fullscreen(s_window, true);
+#endif
   
   s_res_bitham_42_bold = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
   s_res_gothic_18_bold = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
-  s_res_bitham_30_black = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK);
+
   // s_textlayer_title
   s_textlayer_title = text_layer_create(GRect(0, 0, 144, 20));
   text_layer_set_text(s_textlayer_title, s_Destination);
@@ -36,6 +59,12 @@ static void initialise_ui(void) {
   text_layer_set_text(s_textlayer_subtitle, s_DayOfWeek);
   text_layer_set_text_alignment(s_textlayer_subtitle, GTextAlignmentCenter);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_subtitle);
+  
+  // s_textlayer_destination
+  s_textlayer_destination = text_layer_create(GRect(0, 135, 144, 30));
+  text_layer_set_text(s_textlayer_destination,s_Station);
+  text_layer_set_text_alignment(s_textlayer_destination, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_destination);
   
   // s_textlayer_time
   s_textlayer_time = text_layer_create(GRect(13, 51, 119, 51));
@@ -50,13 +79,6 @@ static void initialise_ui(void) {
   text_layer_set_text_alignment(s_textlayer_desc, GTextAlignmentCenter);
   text_layer_set_font(s_textlayer_desc, s_res_gothic_18_bold);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_desc);
-  
-  // s_textlayer_destination
-  s_textlayer_destination = text_layer_create(GRect(26, 135, 100, 30));
-  text_layer_set_text(s_textlayer_destination,s_Station);
-  text_layer_set_text_alignment(s_textlayer_destination, GTextAlignmentCenter);
-  text_layer_set_font(s_textlayer_destination, s_res_bitham_30_black);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_destination);
 }
 
 static void destroy_ui(void) {
@@ -67,7 +89,6 @@ static void destroy_ui(void) {
   text_layer_destroy(s_textlayer_desc);
   text_layer_destroy(s_textlayer_destination);
 }
-// END AUTO-GENERATED UI CODE
 
 static void handle_window_unload(Window* window) {
   destroy_ui();
@@ -82,6 +103,7 @@ void show_adjust_time(const char *destination, const char *dayofweek, const char
     .unload = handle_window_unload,
   });
   window_stack_push(s_window, true);
+  window_set_click_config_provider(s_window, (ClickConfigProvider) config_provider);
 }
 
 void hide_adjust_time(void) {
